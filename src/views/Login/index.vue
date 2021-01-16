205682247e6dca6083675cbf09b8486a1b62f2ef
李江 <li677220@163.com>
2021-01-16 17:48:17 +0800

    好像没有提交成功

---
+++ src/views/Login/index.vue
@@ -0,0 +1,28 @@
+<template>
+ <div id="login">
+   <el-button type="success" plain>成功按钮</el-button>
+ </div>
+</template>
+
+<script>
+ export default {
+   name: "Login",
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style lang="scss" scoped>
+#login{
+  height: 100vh;
+  width: 100vw;
+  background-color: #bfa;
+}
+ 
+</style>
---
+++ vue.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+module.exports = {
+  // 基本路径
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  // 输出文件目录
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist' : 'devdist',
+  //eslint-loader是否在保存时检查
+  lintOnSave: false,
+
+  //webpack配置
+  // chainWebpack: (config) => {},
+  // configureWebpack: (config) => {
+    // config.resolve = {
+    //   extensions: ['.js','.json','.vue'],
+    //   alias: {
+    //     '@': path.resolve(__dirname,'./src'),
+    //     'public': path.resolve(__dirname,'./public'),
+    //     'components': path.resolve(__dirname,'./src/components'),
+    //     'common': path.resolve(__dirname,'./src/common'),
+    //     'api': path.resolve(__dirname,'./src/api'),
+    //     'views': path.resolve(__dirname,'./src/views'),
+    //     'data': path.resolve(__dirname,'./src/data'),
+    //   }
+    // }
+  // },
+  // 生产环境是否需要生成map文件
+  productionSourceMap: false,
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: false,
+    // 开启css map
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {
+      sass: {
+        prependData: `@import "./src/styles/main.scss";`
+      }
+    },
+    // css modules for all css / pre-processor files
+    modules: false
+  },
+  // parallet: require('os').cpus.length > 1,
+  pwa: {},
+  // webpack-dev-derver相关配置
+  devServer: {
+    open: false, //编译完成是否打开网页
+    host: '0.0.0.0', //指定使用地址，localhost，0.0.0.0代表可以被外界访问
+    port: 8080, //访问端口
+    https: false, //编译失败时刷新界面
+    hot: true, //开启热加载
+    hotOnly: false, //
+    proxy: null, //代理
+    overlay: { //全屏模式下是否显示脚本错误
+      warninggs: true,
+      errors: true
+    },
+    // before = () => {}
+   },
+  // 第三方配置加载
+   pluginOptions: {},
+  //  configureWebpack: {
+	// 	externals: {
+	// 		"vue": "Vue",
+	// 		"vue-router": "VueRouter",
+	// 		"vuex": "Vuex",
+	// 	}
+	// }
+
+}
